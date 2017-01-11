4a42336efc6b80ee7be7d24b06d4e3643523b570
kato1628 <kato@giftee.co>
2017-01-11 22:44:59 +0900

    [add] slate sample #1.

--- src/app/javascripts/renderer/renderer-main.js
+++ src/app/javascripts/renderer/renderer-main.js
@@ -4,7 +4,7 @@
   );
   const React = require('react');
   const ReactDOM = require('react-dom');
-  const MainContent = require('./components/main');
+  const MainContent = require('./components/slate');
   const root = document.getElementById('root');
   ReactDOM.render(React.createElement(MainContent), root);
 })();
