df51a850fb6f52e239c16334266626d5b489b9fe
kato1628 <kato@giftee.co>
2017-02-23 15:38:27 +0900

    [change] refactoring timeline task component.

--- src/app/javascripts/renderer/components/taskbord/timeline-task.jsx
+++ src/app/javascripts/renderer/components/taskbord/timeline-task.jsx
@@ -32,36 +32,19 @@ const propTypes = {
   connectDragSource: PropTypes.func.isRequired
 }
 
-const TimelineTask = class TvTask extends React.Component {
+const timelineTask = class TimelineTask extends React.Component {
   render() {
     const { isDragging, connectDragSource, text } = this.props
-    let index = 1;
-    let tasks = [];
-    let top = 50
 
-    this.props.taskList.document.nodes.map((block) => {
-      if (block.text != "") {
-        let height = 49 * block.data.get("requiredTime", 60) / 60
-        let style = {
-          top: top.toString() + 'px',
-          height: height.toString() + 'px'
-        };
-        tasks.push(<div key={block.key} className={block.data.get("done", false) ? "task done" : "task"} style={style}><span>{block.text}</span></div>);
-        top = top + height + 1
-      }
-    })
-
-    if (tasks) {
-      return connectDragSource(
-        <td className="tv-task">
-          {tasks}
-          <div className="nowmarker"></div>
-        </td>
-      );
-    } else {
-      return null;
-    }
+    return connectDragSource(
+      <div
+        key={this.props.block.key}
+        className={this.props.block.data.get("done", false) ? "task done" : "task"}
+        style={this.props.style}>
+        <span>{this.props.block.text}</span>
+      </div>
+    )
   }
 }
 
-module.exports = DragSource(Types.CARD, taskSource, collect)(TimelineTask);
+module.exports = DragSource(Types.CARD, taskSource, collect)(timelineTask);
--- src/app/javascripts/renderer/components/taskbord/timeline-viewport.jsx
+++ src/app/javascripts/renderer/components/taskbord/timeline-viewport.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import _ from 'lodash';
-import TvTask from './timeline-task'
 import HTML5Backend from 'react-dnd-html5-backend';
 import { DragDropContext } from 'react-dnd';
+import Task from './timeline-task'
 
 const TimelineViewport = class TimelineViewport extends React.Component {
+
+  // make task panel html.
+  renderTasks(){
+    let tasks = [];
+    let top = 50
+    this.props.taskList.document.nodes.map((block) => {
+      if (block.text != "") {
+        let height = 49 * block.data.get("requiredTime", 60) / 60
+        let style = {
+          top: top.toString() + 'px',
+          height: height.toString() + 'px'
+        };
+        tasks.push(<Task block={block} style={style}/>)
+        top = top + height + 1
+      }
+    })
+    return tasks.length > 0 ? tasks : null
+  }
+
   render() {
     return (
       <div id="timeline-viewport" className="col-md-5 col-sm-6 hidden-xs">
@@ -12,11 +31,22 @@ const TimelineViewport = class TimelineViewport extends React.Component {
           <tbody>
             <tr height="1">
               <td className="tv-time"></td>
-              <TvMarker></TvMarker>
+              <td className="tv-marker">
+                {_.map(_.range(1, 14), (m, i) => {
+                  return <div key={i} className="markercell"></div>;
+                })}
+              </td>
             </tr>
             <tr className="">
-              <TvTime></TvTime>
-              <TvTask taskList={this.props.taskList}></TvTask>
+              <td className="tv-time">
+                {_.map(_.range(8, 21), (t, i) => {
+                  return <div key={i} className="time">{t + ":" + "00"}</div>
+                })}
+              </td>
+              <td className="tv-task">
+                {this.renderTasks()}
+                <div className="nowmarker"></div>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -24,29 +54,4 @@ const TimelineViewport = class TimelineViewport extends React.Component {
     );
   }
 }
-
-class TvMarker extends React.Component {
-  render() {
-    return (
-      <td className="tv-marker">
-        {_.map(_.range(1, 14), (m, i) => {
-          return <div key={i} className="markercell"></div>;
-        })}
-      </td>
-    );
-  }
-}
-
-class TvTime extends React.Component {
-  render() {
-    return (
-      <td className="tv-time">
-        {_.map(_.range(8, 21), (t, i) => {
-          return <div key={i} className="time">{t + ":" + "00"}</div>
-        })}
-      </td>
-    );
-  }
-}
-
 module.exports = DragDropContext(HTML5Backend)(TimelineViewport);
