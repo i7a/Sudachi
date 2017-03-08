c5e7cf55576b4612ee9b6c621b2757add18c41da
kato1628 <kato@giftee.co>
2017-03-08 22:09:16 +0900

    [change] date format.

--- src/app/javascripts/renderer/components/taskbord/task-viewport.jsx
+++ src/app/javascripts/renderer/components/taskbord/task-viewport.jsx
@@ -32,7 +32,7 @@ const TaskViewport = class TaskViewport extends React.Component {
     return (
       <div id="task-viewport" className="col-md-5 col-sm-6">
         <div className="editor-area">
-          <div className="ace-line"><span>{this.state.date}</span></div>
+          <div className="ace-line"><span>{moment([this.state.date.slice(0,4), this.state.date.slice(4,6), this.state.date.slice(6,8)].join("-")).format("YYYY.M.D ddd")}</span></div>
           <TaskEditor
             date={this.state.date}
             taskList={this.props.taskList}
